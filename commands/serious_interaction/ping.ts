23ed3ff1ce12ae195231ef15ecaeac04aa7d4554
OscarSix <26691547+KiloOscarSix@users.noreply.github.com>
2022-02-18 01:23:44 +0000

    added description for ping command

--- commands/serious_interaction/ping.ts
+++ commands/serious_interaction/ping.ts
@@ -2,6 +2,7 @@ import Discord from "discord.js"
 
 module.exports = {
     commands: "ping",
+    description: "The ping command",
     minArgs: 0,
     maxArgs: 0,
     callback: (message: Discord.Message, args: string[], text: string) => {
