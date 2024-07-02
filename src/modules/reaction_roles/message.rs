7c61bbd9d85e4b8464025c7748566fa2b7adc2ae
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-07-02 02:21:14 +0100

    update reaction message for colour roles

--- src/modules/reaction_roles/message.rs
+++ src/modules/reaction_roles/message.rs
@@ -1,21 +1,31 @@
 use async_trait::async_trait;
 use serenity::all::{
     ChannelId, CommandInteraction, Context, CreateCommand, CreateEmbed, CreateInteractionResponse,
-    CreateInteractionResponseMessage, EditMessage, Mentionable, MessageId, Permissions, RoleId,
+    CreateInteractionResponseMessage, CreateMessage, Mentionable, Permissions, RoleId,
 };
 use zayden_core::SlashCommand;
 
 use crate::Error;
 
-const LFG_ROLE: RoleId = RoleId::new(944486978693509150);
-const DESTINY2_ROLE: RoleId = RoleId::new(931990930385424445);
-const FORTNITE_ROLE: RoleId = RoleId::new(1172143188656541746);
-const GENSHIN_ROLE: RoleId = RoleId::new(1149110098917064816);
-const MINECRAFT_ROLE: RoleId = RoleId::new(1121081685786767440);
-const TF2_ROLE: RoleId = RoleId::new(1149110022203265065);
+const RED_ROLE: RoleId = RoleId::new(923681972600045618);
+const PURPLE_ROLE: RoleId = RoleId::new(932007593738645584);
+const BLUE_ROLE: RoleId = RoleId::new(932007591943491594);
+const GREEN_ROLE: RoleId = RoleId::new(932007590395797535);
+const YELLOW_ROLE: RoleId = RoleId::new(932007475144720424);
+const ORANGE_ROLE: RoleId = RoleId::new(932007389937422408);
+const BROWN_ROLE: RoleId = RoleId::new(1257494437370794055);
+const WHITE_ROLE: RoleId = RoleId::new(932007595303112714);
+const BLACK_ROLE: RoleId = RoleId::new(932007597006024745);
+
+const CHANNEL_ID: ChannelId = ChannelId::new(932004859899691018);
+// const MESSAGE_ID: MessageId = MessageId::new(1256772349777285201);
 
 fn message() -> CreateEmbed {
-    CreateEmbed::new().title("Game Roles").description(format!("React to this message to get the roles for the games you play!\n\n<:ping:1148819291358105711> ● {}\n\n<:Cayde6ThumbsUp:940629512171970600> ● {}\n<a:fortnite:1172142530914172939> ● {}\n<a:genshin:1150867466805858364> ● {}\n<a:AnimatedMCSheep:1149108483967426560> ● {}\n<:tf2:1149109383180066857> ● {}", LFG_ROLE.mention(), DESTINY2_ROLE.mention(), FORTNITE_ROLE.mention(), GENSHIN_ROLE.mention(), MINECRAFT_ROLE.mention(), TF2_ROLE.mention()))
+    CreateEmbed::new()
+        .title("Colour Roles")
+        .description(format!(
+            "React to this message to get a colour role.\n\n🔴 ● {}\n🟣 ● {}\n🔵 ● {}\n🟢 ● {}\n🟡 ● {}\n🟠 ● {}\n🟤 ● {}\n⚪ ● {}\n⚫ ● {}", RED_ROLE.mention(), PURPLE_ROLE.mention(), BLUE_ROLE.mention(), GREEN_ROLE.mention(), YELLOW_ROLE.mention(), ORANGE_ROLE.mention(), BROWN_ROLE.mention(), WHITE_ROLE.mention(), BLACK_ROLE.mention()
+        ))
 }
 
 pub struct ReactionRoleMessageCommand;
@@ -23,11 +33,8 @@ pub struct ReactionRoleMessageCommand;
 #[async_trait]
 impl SlashCommand<Error> for ReactionRoleMessageCommand {
     async fn run(ctx: &Context, interaction: &CommandInteraction) -> Result<(), Error> {
-        let channel_id = ChannelId::new(931986133762588752);
-        let message_id = MessageId::new(1256772349777285201);
-
-        let _ = channel_id
-            .edit_message(ctx, message_id, EditMessage::new().embed(message()))
+        let _ = CHANNEL_ID
+            .send_message(ctx, CreateMessage::new().embed(message()))
             .await;
 
         interaction
