ca210e85c68a82255e2c67063006fa9ae0b37d14
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-09-26 03:37:16 +0100

    deleted redacted db

--- migrations/20240707230810_create_family_table.up.sql
+++
@@ -1,9 +0,0 @@
--- Add up migration script here
-CREATE TABLE family (
-    id BIGINT PRIMARY KEY,
-    username TEXT NOT NULL,
-    partner_ids BIGINT[] NOT NULL DEFAULT '{}',
-    parent_ids BIGINT[] NOT NULL DEFAULT '{}',
-    children_ids BIGINT[] NOT NULL DEFAULT '{}',
-    blocked_ids BIGINT[] NOT NULL DEFAULT '{}'
-);
