8b17d18bd5c60af851d9fd5b84464549ef59cdf1
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-10-18 02:57:55 +0100

    Add support for Postgres database connection pool

--- src/modules/lfg/modal.rs
+++ src/modules/lfg/modal.rs
@@ -1,12 +1,18 @@
 use serenity::all::{Context, CreateInteractionResponse, ModalInteraction};
+use sqlx::Postgres;
 
+use crate::sqlx_lib::PostgresPool;
 use crate::Result;
+
+use super::LfgPostTable;
 
 pub struct LfgCreateModal;
 
 impl LfgCreateModal {
     pub async fn run(ctx: &Context, interaction: &ModalInteraction) -> Result<()> {
-        lfg::LfgCreateModal::run(ctx, interaction).await?;
+        let pool = PostgresPool::get(ctx).await;
+
+        lfg::LfgCreateModal::run::<Postgres, LfgPostTable>(ctx, interaction, &pool).await?;
 
         interaction
             .create_response(ctx, CreateInteractionResponse::Acknowledge)
