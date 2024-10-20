48fd5fd0f9f8fcb72a9c2d3ce33edafa6deafdfb
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-10-20 18:32:00 +0100

    Create voice_channels table migration script

--- migrations/20241019200941_create_voice_channels_table.up.sql
+++ migrations/20241019200941_create_voice_channels_table.up.sql
@@ -2,6 +2,7 @@
 CREATE TABLE voice_channels (
     id BIGINT PRIMARY KEY NOT NULL,
     owner_id BIGINT NOT NULL,
-    trusted_ids BIGINT[] NOT NULL,
-    password TEXT
+    trusted_ids BIGINT[] NOT NULL DEFAULT '{}',
+    password TEXT DEFAULT NULL,
+    persistent BOOLEAN NOT NULL DEFAULT FALSE
 )
