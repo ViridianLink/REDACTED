2d77f0e8f215f89f21a95451f6d320986a857df6
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2024-11-11 00:20:01 +0000

    Refactor main.rs to streamline PostgresPool initialization and remove commented-out code

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,10 @@
-// mod chatgpt_lib;
-// pub mod components;
-// pub mod cron;
 mod error;
-// mod global_commands;
-// mod guild_commands;
-// pub mod guilds;
+
 mod handler;
-// mod image_cache;
-// pub mod modals;
-// mod models;
+
 pub mod modules;
 mod sqlx_lib;
-// mod sqlx_lib;
-// pub mod state;
-// mod utils;
 
-// use guild_commands::college_kings::{
-//     goodmorning::GoodMorningLockedUsers, goodnight::GoodNightLockedUsers,
-// };
 use serenity::all::{GatewayIntents, UserId};
 use serenity::Client;
 use sqlx_lib::PostgresPool;
@@ -25,15 +12,15 @@ use std::collections::HashMap;
 use std::env;
 use temp_voice::VoiceStateCache;
 
-// use crate::image_cache::ImageCache;
 pub use error::{Error, Result};
 
-// pub const SERVER_URL: &str = "http://145.40.184.89:8080";
 pub const OSCAR_SIX_ID: UserId = UserId::new(211486447369322506);
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenvy::dotenv()?;
+
+    let pool = PostgresPool::init().await?;
 
     let token = &env::var("DISCORD_TOKEN")?;
 
@@ -42,7 +29,7 @@ async fn main() -> Result<()> {
         .await?;
 
     let mut data = client.data.write().await;
-    data.insert::<PostgresPool>(PostgresPool::init().await?);
+    data.insert::<PostgresPool>(pool);
     data.insert::<VoiceStateCache>(HashMap::new());
     drop(data);
 
